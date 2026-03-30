a5d22c2c053973610b7c5ab84f5e277fec929ae5
Sowmya <sowmya.pendota5@gmail.com>
2026-03-31 00:13:54 +0530

    Arthimetic Operators

---
+++ src/com/flm/ArthimeticOperators.java
@@ -0,0 +1,21 @@
+package com.flm;
+
+public class ArthimeticOperators {
+		public static void main(String[] args) {
+			int a=17;
+			int b=3;
+			int add=a+b;
+			String c="sowmya";
+			String d="Rohan";
+			String total=c+d;
+			System.out.println(a+b);
+			System.out.println(a-b);
+			System.out.println(a*b);
+			System.out.println(a/b);
+			System.out.println(a%b);
+			System.out.println(total);
+			String hi="c"+"d";
+			System.out.println(hi);
+		}
+
+}
