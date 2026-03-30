fd7303c8e4be0dead1ebe0d3d5700fbca0f2193d
Sowmya <sowmya.pendota5@gmail.com>
2026-03-31 00:16:00 +0530

    Arthimetic operators

--- src/com/flm/Database/Database.java
+++
@@ -1,8 +0,0 @@
-package com.flm.Database;
-
-public class Database {
-	public int data=123;
-	public static void hi() {
-		System.out.println("Connecting to database....");
-	}
-}
--- src/com/flm/Payment/payment.java
+++
@@ -1,5 +0,0 @@
-package com.flm.Payment;
-
-public class payment {
-
-}
