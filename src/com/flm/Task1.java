3c86d92229d089ea1c7b30d9df6eb4ff08760866
Sowmya <sowmya.pendota5@gmail.com>
2026-04-02 22:43:36 +0530

    Task1

---
+++ src/com/flm/Task1.java
@@ -0,0 +1,8 @@
+package com.flm;
+
+public class Task1 {
+	public static void main(String[] args) {
+		int m=70;
+		System.out.println((m>90 && m<=100)?"Grade A":(m>70 && m<=90)?"Grade B":(m>50 && m<=70)?"Grade C":"Fail");
+	}
+}
